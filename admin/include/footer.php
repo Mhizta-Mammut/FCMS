b44422e2bc8c956ec048f0c9a203e3c2c321352f
Hamisu Muhammad <csc152151@students.umyu.edu.ng>
2020-11-21 14:59:21 +0100

    first commit

---
+++ admin/include/footer.php
@@ -0,0 +1,5 @@
+<div class="footer">
+	<div class="container text-center">
+		<b class="copyright"><a href="https://www.maaun.net">MAAUN</a> &copy; <?php echo date('Y'); ?> FCMS </b> All rights reserved.
+	</div>
+</div>
